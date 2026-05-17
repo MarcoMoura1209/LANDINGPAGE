aa3e1f0a9715dd904b30381e6c0448f925d2a12e
Marco Moura <marcomoura27052009@gmail.com>
2026-05-17 14:54:07 -0300

    Update admin URL configuration to use environment variable for flexibility

--- MouraWeb/urls.py
+++ MouraWeb/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from decouple import config
 
+
+admin_url = config('ADMIN_URL', default='admin/')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_url, admin.site.urls),
     path('', include('core.urls')),
 ]
 
